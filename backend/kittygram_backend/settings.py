49a671aabbe610697a98917fc8ab8cb521eb56b4
Денис Чередниченко <promo.ekb.cherednichenko.d@gmail.com>
2023-07-25 22:56:43 +0300

    Фикс замечаний ревью

--- backend/kittygram_backend/settings.py
+++ backend/kittygram_backend/settings.py
@@ -11,7 +11,7 @@ SECRET_KEY = os.getenv('SECRET_KEY', 'key-missing')
 
 DEBUG = os.getenv('DEBUG_VALUE', 'False').lower() == 'true'
 
-ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS').split(',')
+ALLOWED_HOSTS = os.getenv('ALLOWED_HOSTS', 'allow_hosts_missing').split(',')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
